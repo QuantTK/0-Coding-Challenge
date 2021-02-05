90b93bdaeaaabdf091f3acc6752e57e03382c0d2
QuantTK <kgasagotk@gmail.com>
2021-02-05 19:57:02 +0200

    This should be right

--- Task 0.9.py
+++ Task 0.9.py
@@ -1,22 +1,10 @@
 def get_vowels(string):
+    vowels = ["a","A","e","E","i","I","o","O","u","U"]
+    lis = []
     for i in string:
-        if i == "A":
-            print("A")
-        elif i =="a":
-            print("a")
-        elif i == "E":
-            print("E")
-        elif i == "e":
-            print("e")
-        elif i == "I":
-            print("i")
-        elif i == "O":
-            print("O")
-        elif i == "o":
-            print("o")
-        elif i == "U":
-            print("U")
-        elif i == "u":
-            print("u")
+        for j in vowels:
+            if j == i:
+                lis.append(j)
+    print(', '.join(lis))
 
-get_vowels("TlhAkodIso")
+get_vowels("lolOabanEilwe")
