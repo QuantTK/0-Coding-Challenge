069ec309a07fd8dadf09d101b36b4203ad240894
QuantTK <kgasagotk@gmail.com>
2021-02-08 21:25:47 +0200

    This is it

--- Task 0.10.py
+++ Task 0.10.py
@@ -5,7 +5,13 @@ def common_characters(string_a,string_b):
         for j in string_a:
             if j == i:
                 lis.append(j)
-                
-    print("Common Letters: " + ', '.join(lis))
+
+    unique_lis = []
+    for word in lis:
+        if word not in unique_lis:
+            unique_lis.append(word)
+
     
-common_characters("House","computers")
+    print("Common Letters: " + ', '.join(unique_lis))
+    
+common_characters("aaaaaa","a")
