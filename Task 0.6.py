bde788505c9cf2f222da454c97afca23e7ef9b09
QuantTK <kgasagotk@gmail.com>
2021-02-08 18:51:20 +0200

    The boy is nice

--- Task 0.6.py
+++ Task 0.6.py
@@ -1,11 +1,13 @@
 def get_maximum_value(a,b,c):
+    
     max = a
     if b > max:
         max = b
         if c > max:
-         max = c
+            max = c
     elif c > max:
-         max = c
+        max = c
     return max
+
 max_val = get_maximum_value(0,0,1)
 print(max_val)
