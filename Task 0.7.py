d2bf96c9e71467b5d3271be5e4c0a268447c682c
Tlhakodiso Kgasago <kgasagotk@gmail.com>
2021-03-03 15:09:00 +0200

    Update Task 0.7.py

--- Task 0.7.py
+++ Task 0.7.py
@@ -1,10 +1,10 @@
 
 def fahreinheit_to_celsius(temp):
-    celsius = (temp - 32)*5/9
+    celsius = (temp - 32) * 5 / 9
     return celsius
 
 
 def celsius_to_fahrenheit(num):
-    fahren = num*(9/5) + 32
+    fahren = num * (9 / 5) + 32
     return fahren
 
