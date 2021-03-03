52998ed7cb2f4e5a68fc12f0b81367027aa67d6d
Tlhakodiso Kgasago <kgasagotk@gmail.com>
2021-03-03 09:39:07 +0200

    Update Task 0.5.py

--- Task 0.5.py
+++ Task 0.5.py
@@ -3,5 +3,3 @@ def get_triangle_area(x,y,z):
     semi_perimeter = (x + y + z)*(1/2)   
     area = math.sqrt((semi_perimeter*(semi_perimeter-x)*(semi_perimeter-y)*(semi_perimeter-z))) # Heroins formula for area of a triangle with 3 sides
     return area
-area_var = get_triangle_area(3,2,4)
-print(area_var)
