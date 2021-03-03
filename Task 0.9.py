9542e718682d568baabdcb11b25e36fc10029a67
Tlhakodiso Kgasago <kgasagotk@gmail.com>
2021-03-03 11:31:37 +0200

    Update Task 0.9.py

--- Task 0.9.py
+++ Task 0.9.py
@@ -1,9 +1,9 @@
 def get_vowels(string):
-    vowels = ["a","A","e","E","i","I","o","O","u","U"]
+    vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]
     lis = []
     for i in string:
         for j in vowels:
             if j == i:
                 lis.append(j)
-    print(', '.join(lis))
+    print(", ".join(lis))
 
